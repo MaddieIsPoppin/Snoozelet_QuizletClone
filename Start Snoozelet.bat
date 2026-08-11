a8b297a4ba28a0d640e70c635fc151e7440d102a
MaddieIsPoppin <Madisonv.versfeld@gmail.com>
2026-08-12 00:30:32 +0200

    Minimize Windows server launcher

--- Start Snoozelet.bat
+++ Start Snoozelet.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d "C:\Snoozelet\Snoozelet_inside"
+cd /d "%~dp0"
 
+start "Snoozelet Server" /min cmd.exe /c "npm.cmd start"
+timeout /t 3 /nobreak >nul
 start "" http://localhost:3000
-
-npm.cmd start
